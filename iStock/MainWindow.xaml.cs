bd5b377295f7b0674aa3244bd4d179b8efce3be8
hamzi abu khalla <hamziak@gmail.com>
2021-05-12 17:14:13 +0300

    pc164

--- iStock/MainWindow.xaml.cs
+++ iStock/MainWindow.xaml.cs
@@ -1,28 +1,75 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using MahApps.Metro.Controls;
+using System;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace iStock
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : MetroWindow
     {
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void MetroWindow_ContentRendered(object sender, EventArgs e)
+        {
+            GO();
+        }
+        private void GO()
+        {
+            var lw = this.Dispatcher.BeginInvoke(new Action(() =>
+            {
+                GetRecords();
+            }));
+        }
+
+        private void GetRecords()
+        {
+            //using (var db = new Model1())
+            //{
+            //    if (cbAll.IsChecked != true)
+            //    {
+            //        LD = db.Departments.Where(tt => tt.Status == "Active").ToList();
+            //    }
+            //    else
+            //    {
+            //        LD = db.Departments.ToList();
+            //    }
+            //}
+            //GBDepers.Header = LD.Count.ToString();
+            //DGDepers.ItemsSource = LD;
+        }
+
+        private void TbSearch_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
+        {
+
+        }
+
+        private void cbAll_Checked(object sender, System.Windows.RoutedEventArgs e)
+        {
+
+        }
+
+        private void cbAll_Unchecked(object sender, System.Windows.RoutedEventArgs e)
+        {
+
+        }
+        private void OnHyperlinkClick(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void DGMaterials_MouseDoubleClick(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void BExitk_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+
         }
     }
 }
